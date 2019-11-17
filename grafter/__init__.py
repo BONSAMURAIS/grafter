26ce5b10fd89f275a3d8c63419c8e309b3c5884e
unknown <miguelfastudillo@gmail.com>
2019-11-17 19:38:07 +0100

    testing grafter

--- grafter/__init__.py
+++ grafter/__init__.py
@@ -1,1 +1,4 @@
 __version__ = (0, 1)
+
+def generate_all():
+    print('hello world')
--- grafter/bin/grafter_cli.py
+++ grafter/bin/grafter_cli.py
@@ -12,14 +12,16 @@ Options:
 
 """
 from docopt import docopt
-from your_name_here import some_wrapper_function
+from grafter import generate_all
 import sys
 
 
 def main():
+    print('try grafter cli')
     try:
         args = docopt(__doc__, version='Version number for *this* CLI')
-        some_wrapper_function(args)
+        # some_wrapper_function(args)
+        generate_all()
     except KeyboardInterrupt:
         print("Terminating CLI")
         sys.exit(1)
